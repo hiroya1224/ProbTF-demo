1e036d039b4f228dd96d4db138267573f7efc508
Hiroya Sato <h-sato@jsk.imi.i.u-tokyo.ac.jp>
2026-08-04 20:41:24 +0900

    grape_param_estim: remove superseded ensemble pipeline

--- ros/examples/grape-param-estim/scripts/grape_evaluate_pid_proposals.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-from grape_param_estim.pid_proposal_cli import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
--- ros/examples/grape-param-estim/scripts/grape_run_strong_constraint_experiment.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from grape_param_estim.strong_constraint_cli import main
-
-
-if __name__ == "__main__":
-    main()
